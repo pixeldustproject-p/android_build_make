f3cfee911d455cd2217253ac50d3169003d483c5
Steve Kondik <steve@cyngn.com>
2016-11-08 12:51:59 -0800

    build: Require devices to opt-in for SDCLANG
     * Only devices which see a measureable benefit should use this
       since it requires more resources to build.
     * Set TARGET_USE_SDCLANG to true in your BoardConfig if your
       device benefits from it.
    
    Change-Id: I12ee20b0627ff0ca121d653cde482e5449a768cc

--- core/binary.mk
+++ core/binary.mk
@@ -408,7 +408,9 @@ endif
 
 ifeq ($(SDCLANG),true)
     ifeq ($(my_sdclang),)
-        my_sdclang := true
+        ifeq ($(TARGET_USE_SDCLANG),true)
+            my_sdclang := true
+        endif
     endif
 endif
 
